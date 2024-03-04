eea456e16358ae893125e91826a1e7f5d3a37349
ppsm288 <ppsm288@gmail.com>
2024-03-04 00:08:51 -0600

    f

--- terraform/acr.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_container_registry" "terratest" {
-  name                     = "terraacr789" 
-  resource_group_name      = azurerm_resource_group.test.name
-  location                 = azurerm_resource_group.test.location
-  sku                      = "Basic" 
-  admin_enabled            = true 
-  
-}
